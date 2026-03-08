957db03537fd6187995a7add59a7518433ecf11c
Lucas Rios <lucasrius@gmail.com>
2026-03-08 19:36:00 -0300

    Second ok

--- cloudguard/api/v1/endpoints.py
+++ cloudguard/api/v1/endpoints.py
@@ -1,20 +1,10 @@
-from django.shortcuts import get_object_or_404
 from ninja import Router
-# from core.models import K8sCluster
-from .schemas import ClusterIn, ClusterOut
+from typing import List
+from core.models import K8sCluster
+from .schemas import K8sClusterSchema
 
 router = Router()
 
-# @router.post("/clusters", response={201: ClusterOut})
-# def create_cluster(request, data: ClusterIn):
-#     # O Django 5.2 aceita o unpacking direto do Pydantic
-#     cluster = K8sCluster.objects.create(**data.dict())
-#     return 201, cluster
-
-# @router.get("/clusters", response=list[ClusterOut])
-# def list_clusters(request):
-#     return K8sCluster.objects.all()
-
-# @router.get("/clusters/{cluster_id}", response=ClusterOut)
-# def get_cluster(request, cluster_id: int):
-#     return get_object_or_404(K8sCluster, id=cluster_id)
+@router.get("/clusters", response=List[K8sClusterSchema])
+def list_clusters(request):
+    return K8sCluster.objects.all()
--- cloudguard/api/v1/schemas.py
+++ cloudguard/api/v1/schemas.py
@@ -1,18 +1,8 @@
 from ninja import Schema
 from datetime import datetime
-from typing import Optional
 
-class ClusterIn(Schema):
-    """Dados para criar um novo cluster"""
-    name: str
-    api_endpoint: str
-    provider_info: dict = {}
-
-class ClusterOut(Schema):
-    """Dados que a API retorna (com ID e Datas)"""
+class K8sClusterSchema(Schema):
     id: int
     name: str
     api_endpoint: str
-    provider_info: dict
-    is_active: bool
     created_at: datetime
